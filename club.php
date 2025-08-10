2f8f24adc063a8217630dfae78e286171a412465
Pho Hoang <phonhhgcd230181@fpt.edu.vn>
2025-08-10 20:47:41 +0700

    clb story

--- club.php
+++ club.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,6 +11,7 @@
     <link rel="icon" href="img/AVATA-GreFC.png">
     <link rel="stylesheet" href="style.css?v=2">
 </head>
+
 <body>
     <header class="navbar">
 
@@ -38,7 +40,7 @@
             <h1>GRE FC</h1>
             <h3>University of Greenwich Foothball Club</h3>
             <p>Established in 2018, GreFC represents the spirit and excellence of the University of Greenwich
-             in competitive football while building a strong community of student athletes.
+                in competitive football while building a strong community of student athletes.
             </p>
 
             <div class="club-stats">
@@ -62,5 +64,28 @@
             </div>
         </div>
     </section>
+
+    <section class="story-section">
+        <div class="story-para">
+            <h1 style="color: #002f9d">Our Story</h1>
+            <p style="font-size: large;">GreFC was born from the passion of a small group of University of Greenwich students who began playing together as a small football team in 2018. What started as casual kickabouts soon grew into a more organised and competitive group, driven by a shared love for the game.
+                <br><br>
+
+                In 2021, GreFC was officially recognised as a club under the University of Greenwich, marking the start of a new chapter where we could represent the university, attract more members, and strengthen our football community.
+                <br><br>
+
+                Built on three core principles — excellence in sport, inclusivity for all skill levels, and building lasting friendships — our club remains committed to providing a welcoming environment for everyone.
+                <br><br>
+
+                From just 12 founding members, GreFC has grown into one of the most successful and respected clubs at the University of Greenwich, competing in the university league and fostering a strong sense of belonging on and off the pitch.
+                <br><br>
+            </p>
+        </div>
+
+        <div class="story-image">
+            <img src="img/clbstory.jpg" alt="clb picture">
+        </div>
+    </section>
 </body>
+
 </html>
