1033190fd3397a4f2644ba62e7b338cd2516a889
Pho Hoang <phonhhgcd230181@fpt.edu.vn>
2025-08-10 19:22:17 +0700

    html for club intro

---
+++ club.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gre Fc's players</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+        integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="icon" href="img/AVATA-GreFC.png">
+    <link rel="stylesheet" href="style.css?v=2">
+</head>
+<body>
+    <header class="navbar">
+
+        <div class="container">
+
+            <img class="logo" src="img/AVATA-GreFC.png" alt="logo of Gre FC">
+            <nav>
+                <a href="index.php">New</a>
+                <a href="players.php">Player</a>
+                <a href="club.php">Club</a>
+                <a href="more.php">More</a>
+            </nav>
+        </div>
+        <!-- close container -->
+
+        <div class="auth-section">
+            <span> <i class="fa-solid fa-globe"> </i> EN</span>
+            <a href="#">Sign In</a>
+        </div>
+        <!-- close auth-section -->
+    </header>
+
+    <section class="club-section">
+        <div class="club-container">
+            <img src="/img/AVATA-GreFC.png" alt="logo of Gre FC">
+            <h1>GRE FC</h1>
+            <h3>University of Greenwich Foothball Club</h3>
+            <p>Established in 2018, GreFC represents the spirit and excellence of the University of Greenwich
+             in competitive football while building a strong community of student athletes.
+            </p>
+
+            <div class="club-starts">
+                <div class="stat-card">
+                    <i class="fa-regular fa-calendar"></i>
+                    <h4>2018</h4>
+                    <span>Founded</span>
+                </div>
+
+                <div class="stat-card">
+                    <i class="fa-solid fa-users"></i>
+                    <h4>30+</h4>
+                    <span>members</span>
+                </div>
+
+                <div class="stat-card">
+                    <i class="fa-solid fa-location-dot"></i>
+                    <h4>Da Nang</h4>
+                    <span>Base</span>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
